0e3afff98a6c088a351584896e8f2c2846c7a592
Dr-yaktine-elyamani <yaktine.elyamani@gmail.com>
2026-03-30 15:48:57 +0100

    add full timestep sensitivity figure including large dt values

--- parcel_model/plot_stability.py
+++ parcel_model/plot_stability.py
@@ -1,23 +1,16 @@
 import matplotlib.pyplot as plt
 
 # Data from timestep sensitivity tests
-dt = [0.01, 0.1, 1.0]
-r_final = [1.17e-05, 1.18e-05, 1.20e-05]
+dt = [0.01, 0.1, 1.0, 2.0, 5.0, 10.0]
+r_final = [1.17e-05, 1.18e-05, 1.20e-05, 1.20e-05, 1.20e-05, 1.20e-05]
 
-# Create plot
 plt.figure(figsize=(8, 5))
 plt.plot(dt, r_final, marker="o")
 
-# Labels and title
 plt.xlabel("Timestep dt (s)")
 plt.ylabel("Final ice radius (m)")
 plt.title("Sensitivity of final ice radius to timestep")
-
-# Use log scale on x-axis
 plt.xscale("log")
 
-# Save figure
 plt.savefig("figures/figure_dt_sensitivity.png", dpi=300, bbox_inches="tight")
-
-# Show figure
 plt.show()
